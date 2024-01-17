3ec8a40726a46c1851816cc29bc0d4badd900d25
LinhVu <Linhvt@example.com>
2024-01-17 20:02:45 +0700

    feat-ui:dashboard

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,7 +1,8 @@
+import Dashboard from '@/pages/Dashboard';
 import UserManager from '@/pages/UserManager';
 
 const publicRoutes = [
-    { path: 'dashboard', component: UserManager },
+    { path: 'dashboard', component: Dashboard },
     { path: 'manager-users', component: UserManager },
     { path: 'manager-courses', component: UserManager },
     { path: 'manager-comments', component: UserManager },
