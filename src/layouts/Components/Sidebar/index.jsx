f2ba11ef3af5b96cfa7f22803f08602fcffbbf42
LinhVu <Linhvt@example.com>
2024-02-21 19:37:50 +0700

    fix:ui

--- src/layouts/Components/Sidebar/index.jsx
+++ src/layouts/Components/Sidebar/index.jsx
@@ -18,7 +18,7 @@ function AdminSidebar() {
     ];
 
     return (
-        <div className="w-[280px] fixed top-0 bottom-0 bg-white border-r">
+        <div className="lg:w-[280px] sm:w-[180px] sm:w-[200px]  top-0 bottom-0 bg-white border-r">
             {/* bg-[#111c43] */}
             <div className="flex flex-col">
                 <div className="logo flex items-center justify-center border-b pb-4 p-4">
