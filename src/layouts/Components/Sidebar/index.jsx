1eaa28732701d57862659c4e61b4fa09a614d1df
unknown <longmobietv@gmail.com>
2024-04-30 17:41:01 +0700

    Fix logo app

--- src/layouts/Components/Sidebar/index.jsx
+++ src/layouts/Components/Sidebar/index.jsx
@@ -5,6 +5,8 @@ import classNames from 'classnames/bind';
 
 import styles from './AdminSidebar.module.scss';
 
+import logoImage from '@/assets/images/logo.png';
+
 const cx = classNames.bind(styles);
 
 function AdminSidebar() {
@@ -22,12 +24,7 @@ function AdminSidebar() {
             <div className="flex flex-col">
                 <div className="logo flex items-center justify-center border-b pb-4 p-4">
                     <Link to="/dashboard" className="flex items-center gap-3">
-                        <img
-                            width={35}
-                            style={{ borderRadius: '5px' }}
-                            src="http://localhost/braintech/public/imgs/logo.png"
-                            alt=""
-                        />
+                        <img width={35} style={{ borderRadius: '5px' }} src={logoImage} alt="" />
                         <div style={{ fontWeight: 600, fontSize: '20px' }}>
                             <span style={{ color: '#3dd5a2' }}>Brain</span>
                             <span style={{ color: '#6666ff' }}>Tech</span>
