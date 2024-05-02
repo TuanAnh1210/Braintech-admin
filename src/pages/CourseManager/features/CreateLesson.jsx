4a343c0701f680e428113b6e874bff49775155de
nguyendragon <longmobietv@gmail.com>
2024-05-03 01:05:14 +0700

    Refactor Courses

--- src/pages/CourseManager/features/CreateLesson.jsx
+++ src/pages/CourseManager/features/CreateLesson.jsx
@@ -408,16 +408,6 @@ const VideoYoutubePlayer = ({ url = '' }) => {
     console.log(videoId);
 
     return (
-        // <iframe
-        //     width="100%"
-        //     height="380"
-        //     title="YouTube video player"
-        //     frameBorder="0"
-        //     src={url}
-        //     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        //     referrerPolicy="strict-origin-when-cross-origin"
-        //     allowFullScreen
-        // ></iframe>
         <YouTube
             opts={opts}
             style={{
--- src/pages/CourseManager/features/UpdateLesson.jsx
+++ src/pages/CourseManager/features/UpdateLesson.jsx
@@ -416,16 +416,6 @@ const VideoYoutubePlayer = ({ url = '' }) => {
     }
 
     return (
-        // <iframe
-        //     width="100%"
-        //     height="380"
-        //     title="YouTube video player"
-        //     frameBorder="0"
-        //     src={url}
-        //     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        //     referrerPolicy="strict-origin-when-cross-origin"
-        //     allowFullScreen
-        // ></iframe>
         <YouTube
             opts={opts}
             style={{
